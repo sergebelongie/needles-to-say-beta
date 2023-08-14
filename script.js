4f18ac77a9c02a229e53c2582e0d99bc344cb3dd
Serge Belongie <sergebelongie@users.noreply.github.com>
2023-08-14 19:04:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-// Define your puzzles data
 let puzzles = [];
-
-// Load puzzles data on page load
-fetch('data.csv')
-    .then(response => response.text())
-    .then(data => {
-        let csvLines = data.split('\n');
-        csvLines.shift(); // remove headers
-        csvLines.forEach(line => {
-            let [clue, word1, word2] = line.split(',');
-            puzzles.push({ clue, word1, word2 });
-        });
-        loadPuzzleForToday();
-    });
 
 function loadPuzzleForToday() {
     let today = new Date();
@@ -29,48 +15,53 @@ function loadPuzzleForGivenDate() {
 }
 
 function loadPuzzle(puzzleId) {
-    if (puzzles[puzzleId]) {
-        document.getElementById('clue').textContent = puzzles[puzzleId].clue;
+    let puzzleEntry = puzzles[puzzleId];
+    if (puzzleEntry) {
+        document.getElementById('clue').textContent = puzzleEntry.clue;
     } else {
         document.getElementById('clue').textContent = "No puzzle available for this day.";
     }
 }
 
 function submitGuess() {
-    // Your submit logic here
-}
+    let guess = document.getElementById("guess").value.toLowerCase();
+    let today = new Date();
+    let start = new Date('2023-07-30');
+    let difference = Math.floor((today - start) / (1000 * 60 * 60 * 24));
+    let currentPuzzle = puzzles[difference];
 
-// Additional code for other functionalities...
-
-
-function submitGuess() {
-    const guessedWordPair = document.getElementById('guess').value.toLowerCase().trim();
-    const correctPair = `${currentPuzzle.word1.toLowerCase()} ${currentPuzzle.word2.toLowerCase()}`;
-    
-    const resultDiv = document.getElementById('result');
-
-    if (guessedWordPair === correctPair) {
-        resultDiv.textContent = 'Correct!';
+    if (currentPuzzle && (guess === currentPuzzle.word1.toLowerCase() || guess === currentPuzzle.word2.toLowerCase())) {
+        document.getElementById('result').textContent = "Correct!";
         showSocialShare();
     } else {
-        resultDiv.textContent = 'Try again.';
+        document.getElementById('result').textContent = "Try again!";
     }
 }
 
 function showSocialShare() {
-    const puzzleId = (new Date() - new Date(2023, 6, 30)) / (1000 * 60 * 60 * 24);
-    const shareText = `Needles to Say #${puzzleId} ${document.getElementById('timer').textContent}`;
-    document.getElementById('social-share').style.display = 'block';
+    let socialShareDiv = document.getElementById('social-share');
+    let shareText = `I solved today's puzzle on Needles to Say! #NeedlesToSay`;
     document.getElementById('share-text').textContent = shareText;
+    socialShareDiv.style.display = 'block';
 }
 
-document.getElementById('guess').addEventListener('keydown', function(e) {
-    if (e.key === 'Enter') {
+// Load puzzles data on page load
+fetch('data.csv')
+    .then(response => response.text())
+    .then(data => {
+        let csvLines = data.split('\n');
+        csvLines.shift(); // remove headers
+        csvLines.forEach(line => {
+            let [clue, word1, word2] = line.split(',');
+            puzzles.push({ clue, word1, word2 });
+        });
+        loadPuzzleForToday();
+    });
+
+// Listen for Enter key press on guess input field
+document.getElementById("guess").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
         submitGuess();
     }
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    loadPuzzleForToday();
-});
-
