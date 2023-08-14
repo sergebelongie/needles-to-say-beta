756e50f73835bda422f48b8563738f163c71769c
Serge Belongie <sergebelongie@users.noreply.github.com>
2023-08-14 15:27:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -95,3 +95,13 @@ function copyToClipboard() {
     alert('Result copied to clipboard! Share it on your favorite platform.');
 }
 
+function checkForEnter(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action (form submission, page refresh) if needed
+        event.preventDefault();
+        // Trigger the button click event
+        submitGuess();
+    }
+}
+
