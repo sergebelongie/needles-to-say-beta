e1ec9a1391a5c3be008f7aedfc6769f2c2fc669c
Serge Belongie <sergebelongie@users.noreply.github.com>
2023-08-14 18:45:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,59 @@
 let puzzles = [];
 let currentPuzzle = null;
-let startTime = null;
 
-// Load puzzles from data.csv
-fetch('data.csv')
-    .then(response => response.text())
-    .then(data => {
-        let lines = data.trim().split('\n');
-        for(let i = 1; i < lines.length; i++) {
-            let [clue, word1, word2] = lines[i].split(',');
-            puzzles.push({ clue, word1, word2 });
-        }
-        loadDailyPuzzle();
-    });
+document.addEventListener('DOMContentLoaded', (event) => {
+    fetch('data.csv')
+        .then(response => response.text())
+        .then(data => {
+            const csvLines = data.trim().split("\n");
+            csvLines.shift();  // Remove header
+            csvLines.forEach(line => {
+                const [clue, word1, word2] = line.split(',');
+                puzzles.push({ clue, word1, word2 });
+            });
+            loadPuzzleForToday();
+        });
+});
 
-function loadDailyPuzzle() {
-    let dayOfYear = Math.floor((new Date() - new Date('2023-07-30')) / (1000 * 60 * 60 * 24)) % puzzles.length;
-    currentPuzzle = puzzles[dayOfYear];
-    document.getElementById('dailyClue').textContent = currentPuzzle.clue;
-    startTime = new Date();
+function loadPuzzleForToday() {
+    const today = new Date();
+    const startDay = new Date(2023, 6, 30);  // Month is 0-indexed
+    const daysDiff = Math.floor((today - startDay) / (1000 * 60 * 60 * 24)) % puzzles.length;
+    currentPuzzle = puzzles[daysDiff];
+    document.getElementById('clue').textContent = currentPuzzle.clue;
+}
+
+function loadPuzzleForGivenDate() {
+    const selectedDate = new Date(document.getElementById('override-date').value);
+    const startDay = new Date(2023, 6, 30);  // Month is 0-indexed
+    const daysDiff = Math.floor((selectedDate - startDay) / (1000 * 60 * 60 * 24)) % puzzles.length;
+    currentPuzzle = puzzles[daysDiff];
+    document.getElementById('clue').textContent = currentPuzzle.clue;
 }
 
 function submitGuess() {
-    let guess = document.getElementById('userGuess').value.toLowerCase();
-    let [guessedWord1, guessedWord2] = guess.split(' ');
+    const guessedWordPair = document.getElementById('guess').value.toLowerCase().trim();
+    const correctPair = `${currentPuzzle.word1.toLowerCase()} ${currentPuzzle.word2.toLowerCase()}`;
+    
+    const resultDiv = document.getElementById('result');
 
-    if (guessedWord1 === currentPuzzle.word1.toLowerCase() && guessedWord2 === currentPuzzle.word2.toLowerCase()) {
-        let timeTaken = Math.round((new Date() - startTime) / 1000);
-        let minutes = Math.floor(timeTaken / 60);
-        let seconds = timeTaken % 60;
-        document.getElementById('resultMessage').textContent = `Correct! Time taken: ${minutes}:${seconds}`;
-        // Display social share
-        document.getElementById('socialShare').style.display = 'block';
+    if (guessedWordPair === correctPair) {
+        resultDiv.textContent = 'Correct!';
+        showSocialShare();
     } else {
-        document.getElementById('resultMessage').textContent = 'Try again!';
+        resultDiv.textContent = 'Try again.';
     }
 }
 
-function checkForEnter(event) {
-    if (event.key === 'Enter') {
+function showSocialShare() {
+    const puzzleId = (new Date() - new Date(2023, 6, 30)) / (1000 * 60 * 60 * 24);
+    const shareText = `Needles to Say #${puzzleId} ${document.getElementById('timer').textContent}`;
+    document.getElementById('social-share').style.display = 'block';
+    document.getElementById('share-text').textContent = shareText;
+}
+
+document.getElementById('guess').addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
         submitGuess();
     }
-}
-
-function provideFeedback() {
-    window.location.href = 'https://github.com/sergebelongie/needles-to-say-beta/issues';
-}
-
-function showAbout() {
-    let aboutSection = document.getElementById('aboutSection');
-    if (aboutSection.style.display === 'none') {
-        aboutSection.style.display = 'block';
-    } else {
-        aboutSection.style.display = 'none';
-    }
-}
+});
