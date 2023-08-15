d9fccb74d48a78d5f72babbdf8d9a4db790e5ff5
Serge Belongie <sergebelongie@users.noreply.github.com>
2023-08-15 15:22:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("Game initialized!");
+let betaTesting = false; // Flag for beta testing
+let startTime;
+let endTime;
+let currentPuzzle;
 
-    // TODO: Load CSV data.
-    loadGameData();
-
-    // TODO: Implement game logic.
-    setupGame();
-
-    // TODO: Implement guess checking logic.
-
-    // TODO: Implement beta testing override.
-});
-
-function loadGameData() {
-    // Placeholder: Load the game data from the CSV file.
-    console.log("Loading game data...");
+// Function to load the puzzle based on the date or beta testing input
+function loadPuzzle(id) {
+    // Fetch data from CSV and select puzzle based on ID
+    fetch('data.csv')
+    .then(response => response.text())
+    .then(data => {
+        let lines = data.split('\n');
+        let puzzleIndex = (id - 1) % (lines.length - 1); // Wrap around if ID exceeds number of rows
+        let puzzleData = lines[puzzleIndex + 1].split(',');
+        currentPuzzle = {
+            clue: puzzleData[0],
+            word1: puzzleData[1],
+            word2: puzzleData[2]
+        };
+        document.getElementById('dailyChallengeClue').textContent = currentPuzzle.clue;
+    });
 }
 
-function setupGame() {
-    // Placeholder: Set up the game logic.
-    console.log("Setting up the game...");
-}
-
-function showPage(pageId) {
-    // Hide all pages
-    let pages = document.querySelectorAll('.page');
-    pages.forEach(page => {
-        page.style.display = 'none';
+document.addEventListener('DOMContentLoaded', () => {
+    // Initialize game
+    document.getElementById('startGameBtn').addEventListener('click', () => {
+        document.getElementById('splashScreen').hidden = true;
+        document.getElementById('gameUI').hidden = false;
+        if (betaTesting) {
+            document.getElementById('betaTestUI').hidden = false;
+        }
+        let currentDate = new Date();
+        let startDate = new Date('2023-07-30');
+        let daysDifference = Math.floor((currentDate - startDate) / (1000 * 60 * 60 * 24));
+        loadPuzzle(daysDifference);
+        startTime = Date.now();
     });
 
-    // Show the requested page
-    document.getElementById(pageId).style.display = 'block';
-}
+    // Handle guess submission
+    document.getElementById('submitGuessBtn').addEventListener('click', () => {
+        let guess = document.getElementById('guessInput').value.toLowerCase();
+        if (guess === currentPuzzle.word1.toLowerCase() + ' ' + currentPuzzle.word2.toLowerCase()) {
+            endTime = Date.now();
+            let timeTaken = Math.round((endTime - startTime) / 1000);
+            document.getElementById('gameFeedback').textContent = 'Correct! Well done.';
+            document.getElementById('socialSharing').hidden = false;
+            document.getElementById('puzzleID').textContent = currentPuzzle.clue;
+            document.getElementById('timeTaken').textContent = timeTaken + 's';
+        } else {
+            document.getElementById('gameFeedback').textContent = 'Try again!';
+        }
+    });
+
+    // Handle beta testing puzzle loading
+    if (betaTesting) {
+        document.getElementById('loadPuzzleBtn').addEventListener('click', () => {
+            let puzzleId = document.getElementById('puzzleNumberInput').value;
+            loadPuzzle(puzzleId);
+        });
+    }
+});
