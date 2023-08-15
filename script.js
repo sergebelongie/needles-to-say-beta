b5db74d071453537e28c8fe4355b66e96ef34cf1
Serge Belongie <sergebelongie@users.noreply.github.com>
2023-08-15 22:35:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,43 @@
-let puzzles = [];
-let currentPuzzle = null;
-let startTime;
-let betaTesting = true; // Set this to false for production
-
-async function initializeGame() {
-    await loadPuzzles();
-    if(betaTesting) {
-        document.getElementById('betaPuzzleId').style.display = "block";
-    }
-    loadPuzzle();
+body {
+    font-family: 'Arial', sans-serif;
+    text-align: center;
+    background-color: #fafafa;
 }
 
-async function loadPuzzles() {
-    const response = await fetch("Data.csv");
-    const csvText = await response.text();
-    const lines = csvText.split('\n').slice(1);  // Remove header
-    for (let line of lines) {
-        const [clue, word1, word2] = line.split(',');
-        puzzles.push({ clue, word1, word2 });
-    }
+.splash-screen {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.8);
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 }
 
-function loadPuzzle() {
-    const dateDiff = Math.floor((new Date() - new Date("2023-07-30")) / (1000 * 60 * 60 * 24));
-    const puzzleId = dateDiff % puzzles.length;
-    currentPuzzle = puzzles[puzzleId];
-    document.getElementById('clue').textContent = currentPuzzle.clue;
-    startTime = new Date();
+button {
+    padding: 10px 20px;
+    margin: 10px;
+    border: none;
+    background-color: #4CAF50;
+    color: white;
+    cursor: pointer;
+    transition: background-color 0.3s;
 }
 
-function submitGuess() {
-    const word1Guess = document.getElementById('word1').value.trim().toLowerCase();
-    const word2Guess = document.getElementById('word2').value.trim().toLowerCase();
-    if (word1Guess === currentPuzzle.word1.toLowerCase() && word2Guess === currentPuzzle.word2.toLowerCase()) {
-        const endTime = new Date();
-        const timeTaken = Math.floor((endTime - startTime) / 1000);
-        alert(`Correct! It took you ${timeTaken} seconds.`);
-    } else {
-        alert("Try again!");
-    }
+button:hover {
+    background-color: #45a049;
 }
 
-function copyToClipboard() {
-    const dateDiff = Math.floor((new Date() - new Date("2023-07-30")) / (1000 * 60 * 60 * 24));
-    const timeTaken = Math.floor((new Date() - startTime) / 1000);
-    const text = `I solved Needles to Say puzzle no. ${dateDiff} in ${timeTaken} sec.`;
-    navigator.clipboard.writeText(text);
-    alert("Text copied to clipboard!");
+input {
+    padding: 10px;
+    margin: 10px;
+    border: 1px solid #ccc;
 }
 
-function dismissSplash() {
-    document.getElementById('splash').style.display = 'none';
-}
-
-function showAbout() {
-    alert("Lorem ipsum...");
-}
-
-function showFeedback() {
-    window.open(`mailto:email@example.com?subject=Feedback for Needles to Say&body=Your feedback here...`);
-}
-
-function loadBetaPuzzle() {
-    const puzzleId = document.getElementById('betaPuzzleId').value;
-    currentPuzzle = puzzles[puzzleId];
-    document.getElementById('clue').textContent = currentPuzzle.clue;
+.betaPuzzleId {
+    display: none;
 }
