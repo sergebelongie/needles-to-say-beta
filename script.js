f1afc9262909b369db63b47d6aa30d419843b52a
Serge Belongie <sergebelongie@users.noreply.github.com>
2023-08-15 15:28:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,80 @@
-let betaTesting = false; // Flag for beta testing
+let betaTesting = false;
 let startTime;
 let endTime;
 let currentPuzzle;
 
-// Function to load the puzzle based on the date or beta testing input
-function loadPuzzle(id) {
-    // Fetch data from CSV and select puzzle based on ID
-    fetch('data.csv')
-    .then(response => response.text())
-    .then(data => {
-        let lines = data.split('\n');
-        let puzzleIndex = (id - 1) % (lines.length - 1); // Wrap around if ID exceeds number of rows
-        let puzzleData = lines[puzzleIndex + 1].split(',');
-        currentPuzzle = {
-            clue: puzzleData[0],
-            word1: puzzleData[1],
-            word2: puzzleData[2]
-        };
-        document.getElementById('dailyChallengeClue').textContent = currentPuzzle.clue;
-    });
+function initializeGame() {
+    if (betaTesting) {
+        document.getElementById('betaTestUI').style.display = 'block';
+    }
+    loadPuzzle();
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    // Initialize game
-    document.getElementById('startGameBtn').addEventListener('click', () => {
-        document.getElementById('splashScreen').hidden = true;
-        document.getElementById('gameUI').hidden = false;
-        if (betaTesting) {
-            document.getElementById('betaTestUI').hidden = false;
-        }
-        let currentDate = new Date();
-        let startDate = new Date('2023-07-30');
-        let daysDifference = Math.floor((currentDate - startDate) / (1000 * 60 * 60 * 24));
-        loadPuzzle(daysDifference);
-        startTime = Date.now();
-    });
+function startGame() {
+    document.getElementById('splashScreen').style.display = 'none';
+    document.getElementById('gameUI').style.display = 'block';
+    startTime = Date.now();
+}
 
-    // Handle guess submission
-    document.getElementById('submitGuessBtn').addEventListener('click', () => {
-        let guess = document.getElementById('guessInput').value.toLowerCase();
-        if (guess === currentPuzzle.word1.toLowerCase() + ' ' + currentPuzzle.word2.toLowerCase()) {
-            endTime = Date.now();
-            let timeTaken = Math.round((endTime - startTime) / 1000);
-            document.getElementById('gameFeedback').textContent = 'Correct! Well done.';
-            document.getElementById('socialSharing').hidden = false;
-            document.getElementById('puzzleID').textContent = currentPuzzle.clue;
-            document.getElementById('timeTaken').textContent = timeTaken + 's';
-        } else {
-            document.getElementById('gameFeedback').textContent = 'Try again!';
-        }
-    });
-
-    // Handle beta testing puzzle loading
-    if (betaTesting) {
-        document.getElementById('loadPuzzleBtn').addEventListener('click', () => {
-            let puzzleId = document.getElementById('puzzleNumberInput').value;
-            loadPuzzle(puzzleId);
+function loadPuzzle(id) {
+    fetch('data.csv')
+        .then(response => response.text())
+        .then(data => {
+            let lines = data.split('\n');
+            if (!id) {
+                let currentDate = new Date();
+                let startDate = new Date('2023-07-30');
+                id = Math.floor((currentDate - startDate) / (1000 * 60 * 60 * 24));
+            }
+            let puzzleIndex = (id - 1) % (lines.length - 1);
+            let puzzleData = lines[puzzleIndex + 1].split(',');
+            currentPuzzle = {
+                clue: puzzleData[0],
+                word1: puzzleData[1],
+                word2: puzzleData[2]
+            };
+            document.getElementById('dailyChallengeClue').textContent = currentPuzzle.clue;
         });
+}
+
+function submitGuess() {
+    let guess = document.getElementById('guessInput').value.toLowerCase().trim();
+    if (guess === currentPuzzle.word1.toLowerCase() + ' ' + currentPuzzle.word2.toLowerCase()) {
+        endTime = Date.now();
+        let timeTaken = Math.round((endTime - startTime) / 1000);
+        document.getElementById('gameFeedback').textContent = 'Correct! Well done.';
+        document.getElementById('socialSharing').style.display = 'block';
+        document.getElementById('puzzleID').textContent = currentPuzzle.clue;
+        document.getElementById('timeTaken').textContent = timeTaken;
+    } else {
+        document.getElementById('gameFeedback').textContent = 'Try again!';
     }
-});
+}
+
+function checkInput(event) {
+    if (event.keyCode === 13) {
+        submitGuess();
+    }
+}
+
+function loadCustomPuzzle() {
+    let id = parseInt(document.getElementById('puzzleNumberInput').value);
+    loadPuzzle(id);
+}
+
+function sendFeedback() {
+    let feedback = document.getElementById('feedbackText').value;
+    alert(`Thank you for your feedback! We would send this to email@example.com:\n\n${feedback}`);
+}
+
+function copyToClipboard() {
+    let text = `Needles to Say ${document.getElementById('puzzleID').textContent} in ${document.getElementById('timeTaken').textContent} seconds`;
+    let tempInput = document.createElement('textarea');
+    tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+    tempInput.value = text;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+    alert("Copied to clipboard!");
+}
